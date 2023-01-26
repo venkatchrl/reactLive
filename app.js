17ada606af9c3359c2fac76908175e79c141fc6d
Venkat Marepalli <venkatmarepalli.19@gmail.com>
2023-01-26 21:59:44 +0530

    Igniting our app

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+/*
+WHAT DOES PARCEL DO?
+--------------------
+HMR - hot module reloading
+file watch algorithm
+bundling
+minify
+cleaning our code
+Dev and prod build
+super and fast build algorithm
+image optimisation
+caching whle development
+compression
+compatable with older version of browsers
+HTTPS on dev
+consistent caching algorithm
+zero config
+transitive dependencies
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement(
   "h1",
   { id: "title1" },
-  " Namaste - This is what gets delivered "
+  " Namaste Everyone- This is delivered by parcel bundler "
 );
 const heading2 = React.createElement(
   "h2",
   { id: "title2" },
-  "Namste to React"
+  "Welcome to React"
 );
 const container = React.createElement("div", {id:"container"}, [heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
